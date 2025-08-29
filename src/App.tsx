01ad6c1e96fb4791e639838a95aac48edaf97764
Tynahan <nusilite@gmail.com>
2025-08-29 15:36:45 +0900

    💄 Style(theme): Move scrollbar style to *

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+const SomeBox = () => {
+    return (
+        <div className="w-[200px] h-[200px] border-1 border-dimdim rounded-md">some box</div>
+    )
+}
+
 const App = () => {
     return (
-        <div className="w-full h-full bg-yellow">
+        <div className="w-full h-full">
             random page
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
+            <SomeBox />
         </div>
     )
 }

a7f7a31fe8b0edb68caa47bcc7107fdee237b37c
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-08-30 19:42:01 +0900

    🐛 Fix(sidebar): Fix sidebar width bug

--- src/package/layout/_ExpandableSidebar.tsx
+++ src/package/layout/_ExpandableSidebar.tsx
@@ -18,18 +18,18 @@ type ExpandableSidebarProps = BaseProps & AdditionalProps;
 
 const FloatingSidebar = ({
   doExpand,
-  widthInPixel,
   className,
   gap,
   position,
   children,
   ...props
-}: ExpandableSidebarProps) => {
+}: Omit<ExpandableSidebarProps, "widthInPixel">) => {
   const positionClassName = position === "LEFT" ? "left-0" : "right-0";
-  const widthClassName = doExpand ? "w-screen" : `w-[${widthInPixel}px]`;
+  const widthClassName = doExpand ? "w-screen" : "w-full";
   return (
     <div
       {...props}
+      // style={widthStyle}
       className={`bg-red-400 ${positionClassName} ${widthClassName} absolute shrink-0 ${className}`}
     >
       <Vstack gap={gap}>{children}</Vstack>
@@ -39,9 +39,15 @@ const FloatingSidebar = ({
 
 const ExpandableSidebar = ({ children, ...props }: ExpandableSidebarProps) => {
   const { widthInPixel } = props;
+  const widthStyle = {
+    minWidth: `${widthInPixel}px`,
+  };
   return (
-    <div className={`h-full min-w-[${widthInPixel}px] relative bg-amber-100`}>
-      {/* <FloatingSidebar {...props}>{children}</FloatingSidebar> */}
+    <div
+      style={widthStyle}
+      className={`h-full min-w-[px] relative bg-amber-100`}
+    >
+      <FloatingSidebar {...props}>{children}</FloatingSidebar>
       <p>what</p>
     </div>
   );

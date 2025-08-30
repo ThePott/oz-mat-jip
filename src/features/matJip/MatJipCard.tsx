af751b01e520f9a70303d972d7cd5065de93f88b
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-08-30 20:44:15 +0900

    ✨ Feat(card): Add flip feature to card

--- src/features/matJip/MatJipCard.tsx
+++ src/features/matJip/MatJipCard.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import type { Place } from "../../shared/store/_apiInterfaces";
 
 const MatJipCard = ({ place }: { place: Place }) => {
+  const [isBack, setIsBack] = useState<boolean>(false);
+  const filpIsBackClassName = isBack ? "rotate-y-180" : "";
+  const filpBaseClassName = "transition ease-[cubic-bezier(.01,.81,.09,1.13)]";
+  const flipClassName = `${filpBaseClassName} ${filpIsBackClassName}`;
+
+  const handleClick = () => {
+    setIsBack((prev) => !prev);
+  };
   return (
-    <div className="h-[200px] rounded-md border-1 border-dimdim hover:border-dim p-3 overflow-hidden">
-      {JSON.stringify(place)}
+    <div className={flipClassName} onClick={handleClick}>
+      <div className="h-[200px] rounded-md border-1 border-dimdim hover:border-dim p-3 overflow-hidden">
+        {JSON.stringify(place)}
+      </div>
     </div>
   );
 };

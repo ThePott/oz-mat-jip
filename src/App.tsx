ebcb5170a869fe8e9b159c51da32560640b9df1a
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-08-30 20:25:49 +0900

    🐛 Fix(scroll): Fix to scroll in layout properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import MatJipContent from "./features/matJip/MatJipContent";
 import ExpandableSidebar from "./package/layout/_ExpandableSidebar";
+import FullScreen from "./package/layout/_FullScreen";
 import Hstack from "./package/layout/_Hstack";
 import { useGetAfterMount } from "./shared/services/servicesHooks";
 import useBoundStore from "./shared/store";
@@ -8,31 +9,33 @@ const App = () => {
   const placeArray = useBoundStore((state) => state.placeArray);
   useGetAfterMount("/places");
   return (
-    <Hstack className="w-screen h-screen bg-amber-800">
-      <div className="grow">
-        <MatJipContent placeArray={placeArray} />
-      </div>
-      <ExpandableSidebar widthInPixel={200} position="RIGHT">
-        <p className="border-1 border-dimdim hover:border-dim">
-          sidebar content
-        </p>
-        <p className="border-1 border-dimdim hover:border-dim">
-          sidebar content
-        </p>
-        <p className="border-1 border-dimdim hover:border-dim">
-          sidebar content
-        </p>
-        <p className="border-1 border-dimdim hover:border-dim">
-          sidebar content
-        </p>
-        <p className="border-1 border-dimdim hover:border-dim">
-          sidebar content
-        </p>
-        <p className="border-1 border-dimdim hover:border-dim">
-          sidebar content
-        </p>
-      </ExpandableSidebar>
-    </Hstack>
+    <FullScreen>
+      <Hstack gap={0} className="w-full h-full bg-amber-800 overflow-hidden">
+        <div className="grow overflow-x-hidden overflow-y-scroll">
+          <MatJipContent placeArray={placeArray} />
+        </div>
+        <ExpandableSidebar widthInPixel={200} position="RIGHT">
+          <p className="border-1 border-dimdim hover:border-dim">
+            sidebar content
+          </p>
+          <p className="border-1 border-dimdim hover:border-dim">
+            sidebar content
+          </p>
+          <p className="border-1 border-dimdim hover:border-dim">
+            sidebar content
+          </p>
+          <p className="border-1 border-dimdim hover:border-dim">
+            sidebar content
+          </p>
+          <p className="border-1 border-dimdim hover:border-dim">
+            sidebar content
+          </p>
+          <p className="border-1 border-dimdim hover:border-dim">
+            sidebar content
+          </p>
+        </ExpandableSidebar>
+      </Hstack>
+    </FullScreen>
   );
 };
 export default App;

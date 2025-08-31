f949b316484464765ca2b3fc86e32c500568c43e
Tynahan <nusilite@gmail.com>
2025-09-01 08:56:38 +0900

    ✨ Feat(flip): Add flip card component

--- src/package/flip/FlipCard.tsx
+++ src/package/flip/FlipCard.tsx
@@ -1,29 +1,91 @@
+import { createContext, useContext, type ReactNode } from "react";
 import type { DivProps } from "../layout/htmlInterfaces";
 
+type FlipCardContextProps = Required<
+  Pick<AdditionalFlipCardProps, "isShowingBack">
+>;
+
+const FlipCardContext = createContext<FlipCardContextProps>({
+  isShowingBack: false,
+});
+
+const useFlipCardContext = () => {
+  const context = useContext(FlipCardContext);
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+};
+
+type FlipCardContextProviderProps = FlipCardContextProps & {
+  children: ReactNode;
+};
+
+const FlipCardContexProvider = ({
+  children,
+  ...props
+}: FlipCardContextProviderProps) => {
+  return (
+    <FlipCardContext.Provider value={props}>
+      {children}
+    </FlipCardContext.Provider>
+  );
+};
+
 interface AdditionalFlipCardItemProps {
-  isFront?: boolean;
+  isForBack?: boolean;
 }
 
 type FlipCardItemProps = DivProps & AdditionalFlipCardItemProps;
 const FlipCardItem = ({
-  isFront,
+  isForBack,
   className,
   children,
   ...props
 }: FlipCardItemProps) => {
-  // const
+  const { isShowingBack } = useFlipCardContext();
+  const baseFlipClassName = "backface-visibility";
+  const conditionalFlipClassName =
+    isShowingBack === isForBack ? "rotate-y-180" : "";
+  const flipClassName = `${baseFlipClassName} ${conditionalFlipClassName}`;
   return (
-    <div {...props} className={`${className}`}>
+    <div {...props} className={`${flipClassName} ${className}`}>
       {children}
     </div>
   );
 };
 
-// interface AdditionalFlipCardProps {
-//     isShowingFront
-// }
-const FlipCard = ({ children, ...props }: DivProps) => {
-  return <div {...props}>{children}</div>;
+interface AdditionalFlipCardProps {
+  isShowingBack?: boolean;
+}
+type FlipCardProps = DivProps & AdditionalFlipCardProps;
+const FlipCard = ({
+  isShowingBack = false,
+  children,
+  ...props
+}: FlipCardProps) => {
+  return (
+    <FlipCardContexProvider isShowingBack={isShowingBack}>
+      <div {...props}>{children}</div>;
+    </FlipCardContexProvider>
+  );
 };
+
+FlipCard.Front = ({
+  children,
+  ...props
+}: Omit<FlipCardItemProps, "isForBack">) => (
+  <FlipCardItem isForBack={false} {...props}>
+    {children}
+  </FlipCardItem>
+);
+FlipCard.Back = ({
+  children,
+  ...props
+}: Omit<FlipCardItemProps, "isForBack">) => (
+  <FlipCardItem isForBack={true} {...props}>
+    {children}
+  </FlipCardItem>
+);
 
 export default FlipCard;

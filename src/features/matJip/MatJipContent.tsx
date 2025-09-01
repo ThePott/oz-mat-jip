4095e43eee37cfbee9d50a9733abec95185b6120
Tynahan <nusilite@gmail.com>
2025-09-02 07:59:57 +0900

    🐛 Fix(grid): Separate scroll and grid to minimize grid row height

--- src/features/matJip/MatJipContent.tsx
+++ src/features/matJip/MatJipContent.tsx
@@ -3,10 +3,12 @@ import MatJipCard from "./MatJipCard";
 
 const MatJipContent = ({ placeArray }: { placeArray: Place[] }) => {
   return (
-    <div className="h-full overflow-x-hidden overflow-y-scroll grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-3 pt-3 pl-3">
-      {placeArray.map((place) => (
-        <MatJipCard key={place.id} place={place} />
-      ))}
+    <div className="h-full overflow-x-hidden overflow-y-scroll">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-3 pt-3 pl-3">
+        {placeArray.map((place) => (
+          <MatJipCard key={place.id} place={place} />
+        ))}
+      </div>
     </div>
   );
 };

ddfbf601290297263a3a69a9f2be8dcb1e1c3540
Tynahan <nusilite@gmail.com>
2025-09-01 10:55:16 +0900

    ✨ Feat(card): Add title to the front of card

--- src/features/matJip/MatJipCard.tsx
+++ src/features/matJip/MatJipCard.tsx
@@ -2,18 +2,29 @@ import type { Place } from "../../shared/store/_apiInterfaces";
 import FlipCard from "../../package/flip/FlipCard";
 import { makeUrlPlaces } from "../../shared/services/apiUtils";
 
-const MatJipCard = ({ place }: { place: Place }) => {
+const MatJipCardFront = ({ place }: { place: Place }) => {
   const imageSrc = `${makeUrlPlaces("/")}${place.image.src}`;
   return (
-    <FlipCard className="h-[200px] overflow-hidden rounded-md border-1 border-dimdim hover:border-dimdim ">
-      <FlipCard.Back>{JSON.stringify(place)}</FlipCard.Back>
-      <FlipCard.Front>
+    <FlipCard.Front>
+      <div className="ralative h-full w-full">
         <img
           src={imageSrc}
           alt={place.image.alt}
           className="h-full w-full object-cover"
         />
-      </FlipCard.Front>
+        <p className="absolute bottom-0 w-full p-2 text-center bg-vivid text-vivid-inverted font-semibold">
+          {place.title}
+        </p>
+      </div>
+    </FlipCard.Front>
+  );
+};
+
+const MatJipCard = ({ place }: { place: Place }) => {
+  return (
+    <FlipCard className="h-[200px] overflow-hidden rounded-md border-1 border-dimdim hover:border-dimdim ">
+      <FlipCard.Back>{JSON.stringify(place)}</FlipCard.Back>
+      <MatJipCardFront place={place} />
     </FlipCard>
   );
 };

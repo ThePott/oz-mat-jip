30d1c65dd1a051f6ee320dec551df7e5036eb40b
Tynahan <nusilite@gmail.com>
2025-09-01 10:57:36 +0900

    💄 Style(content): Add padding to Matjip Content

--- src/features/matJip/MatJipContent.tsx
+++ src/features/matJip/MatJipContent.tsx
@@ -3,7 +3,7 @@ import MatJipCard from "./MatJipCard";
 
 const MatJipContent = ({ placeArray }: { placeArray: Place[] }) => {
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-3">
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-3 pt-3 pl-3">
       {placeArray.map((place) => (
         <MatJipCard key={place.id} place={place} />
       ))}
